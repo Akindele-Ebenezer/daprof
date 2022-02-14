f2e95f3a4455c734c36cea4702145b614f8174f1
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-02-14 15:27:12 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="styles/styles.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <section class="sec-1">
+        <div class="sec-1-inner"> 
+            <div>
+                <div class="logo">
+                    LOGO
+                </div>
+                <h1>Daprof Global Resources</h1>
+                <ul>
+                    <li>Blasting</li>
+                    <li>Painting</li>
+                    <li>Hydrotest</li>
+                    <li>Tank Cleaning</li>
+                    <li>Floor Breaking</li>
+                    <li>Screeding</li>
+                    <li>Pop Design</li>
+                    <li>Industrial Painting</li>
+                    <li>General Supplier</li>
+                </ul>
+            </div> 
+            <div>
+                <img src="images/img.jpeg" alt="">
+                <img src="images/img.jpeg" alt="">
+                <img src="images/img.jpeg" alt="">
+                <img src="images/img.jpeg" alt="">
+        </div>
+    </section>
+
+    <section class="sec-2">
+        <div>
+            <h1>Think Better Corrosion Services</h1>
+            <p>Think</p>
+            <div>                
+                <img src="images/img.jpeg" alt="">
+            </div>
+            <h2>Daprof Global Resources</h2>
+        </div>
+    </section>
+
+    <section class="sec-3">
+        <div>
+            <h2>A One-Stop Headquarter for Anti Rust Solutions</h2>
+            <p>
+            A One-Stop Headquarter for Anti Rust Solutions
+            We all at Daprof Global Resources welcome you to our world of Professional corrosion services.
+            <br>
+            We sincerely appreciate your interest in us and we look forward to not only meeting your corrosion services
+            <br>
+            needs but also delivering world class solutions.
+            <br><br>
+            The importance of efficient and effective corrosions control delivery cant be ruled out. <br>
+            For us to get hired, win more clients and partnerships. Building a stronger professional reputation, Daprof
+            Global Resources are equipped with most effective antirust gadgets obtainable globally. <br><br>
+            With Daprof Global Resources, a lasting Professional corrosions control is assured.
+            <br>
+            Warmest Regards
+            <br>
+            <br>
+            Olorunda Oludare E.
+            <br>
+            Managing Director
+            <br>
+            daprofglobalresources@gmail.com
+            </p>
+        </div>
+    </section>
+
+    <section class="sec-4">
+        <div class="sec-inner">
+            <div>
+                <div class="sec-4-inner">
+                    <div>
+                        <div class="sec-4-inner">
+                            <img src="images/img.jpeg" alt=""> 
+                        </div>
+                        <h2>MISSION</h2>
+                    </div>
+                </div>
+                    <p><q>To continuously challenge ourselves to
+                    provide effective and affordable corrosion
+                    control service to our valued clients.
+                    “ To be Earth’s most customer - centric
+                    company, where customer can find and
+                    discover anything they might want in Anti
+                    rust services, and endeavors to offer its customer
+                    the lowest possible prices</q></p>
+                </img>
+            </div>
+            <div>
+                <div class="sec-4-inner">
+                    <div>
+                        <div class="sec-4-inner">
+                            <img src="images/img.jpeg" alt="">
+                        </div>
+                        <h2>VISION</h2>
+                    </div>
+                </div>
+                    <p>To continuously challenge ourselves to
+                    provide effective and affordable corrosion
+                    control service to our valued clients</p>
+                </img>
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
