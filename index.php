7d3af220af63517e18aaed5959bce5dde868eb6e
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-04-04 17:05:36 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -96,5 +96,88 @@
         </div>
     </section>
 
+    <section class="sec-2-wrapper">
+        <div>
+            <center><img src="images/mission.png"></center>
+            <br>
+            <h2>MISSON</h2>
+            <p>To be Earth’s most customer - centric
+            company, where customer can find and
+            discover anything they might want in Anti
+            rust services, and endeavors to offer its customer
+            the lowest possible prices.</p>
+            <small>MORE SERVICES >></small>
+        </div>
+        <div>
+            <center><img src="images/vision.png"></center>
+            <br>
+            <h2>VISION</h2>
+            <p>To continuously challenge ourselves to 
+            provide effective and affordable corrosion 
+            control service to our valued clients.</p>
+            <small>MORE SERVICES >></small>
+        </div>
+    </section>
+
+    <section class="sec-3-wrapper"> 
+            <div>
+                <img src="images/work-1.jpeg"><img src="images/work-2.jpeg">
+            </div>
+            <div>
+                <h3>
+                    <small>ABOUT DAPROF</small>
+                    <br>
+                    <br>
+                    <big>We Build for Your Comfort</big> 
+                </h3>
+                <br>
+                <p>Daprof Global Resources was incorporated in November 15th 2017 under the
+                Companies and Allied Matters Act 1990 
+                and began business operations in November 20th 
+                2017. Our years of building Trust and Confidence 
+                through our relationship is what has shaped our 
+                resilience and stands us out.
+                <br>
+                <br>
+                Daprof Global Resources has to provide the best 
+                corrosion services to our clients by demonstrating 
+                responsiveness, diligence, judgment and building on 
+                our culture of excellence. 
+                <br>
+                Daprof Global Resources strive to be the highest 
+                corrosion services in Nigeria. We will exceed the 
+                expectations of those we partner with by:
+                Being the best service contractor our client have 
+                ever had.
+                <br>
+                <br>
+                Continuously improving on all aspect of our 
+                business while building on our culture of
+                excellence.
+                <br>
+                Our human resources are our greatest assets. We 
+                recognize the individuality and merit of our staffs in 
+                other to deliver their best to our valued clients.
+                <br>
+                Daprof Global Resources success and existence is 
+                built on our obligation to understand and response 
+                effectively to Our Valued Clients’ needs at all times. 
+                To this end, we endeavour to add value to every 
+                service we offer. 
+                <br>
+                <br>
+                Our Office Doors & Phones Are Open Round The 
+                Clock To Offer You - Precise corrosion services. 
+                All The Way.</p>  
+                <small>MORE ABOUT >></small>
+                <br><br>
+                <div class="about-services">
+                    <div><img src="images/on-time.png"><span>Ontime at <br> Services</span></div>
+                    <div><img src="images/24-7.png"><span>24/7 <br> Services</span></div>
+                    <div><img src="images/verified-professionals.png"><span>Verified <br> Professionals</span>
+                </div>
+            </div> 
+    </section>
+
 </body>
 </html>
